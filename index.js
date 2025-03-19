fd586e8e4e899801f172d17ac1994715000609c2
sanandreas11 <andreaandonis@outlook.com>
2025-03-19 14:03:39 +0100

    css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const form = document.getElementById("content")
 const inputField = document.getElementById("userField")
 const saveButton = document.getElementById("save")
 const deleteButton = document.getElementById("delete")
+let savedContent = localStorage.getItem("username")
 
 // SAVE BUTTON
 saveButton.addEventListener("click", (e) => {
@@ -11,16 +12,17 @@ saveButton.addEventListener("click", (e) => {
   const inputContent = inputField.value
   // salvo l'elemento in local storage
   // If statement che controlla che username sia presente e lo mette nell'input al refresh
-  let savedContent = localStorage.getItem("username")
   if (savedContent !== null) {
-    savedContent = inputField.value
+    inputField.value = savedContent
   } else {
     localStorage.setItem("username", inputContent)
   }
   // svuoto l'input field
-  inputField.innerText = ""
+  inputField.value = ""
 })
-
+if (savedContent) {
+  inputField.value = savedContent
+}
 // DELETE BUTTON
 deleteButton.addEventListener("click", (e) => {
   e.preventDefault()
@@ -32,11 +34,16 @@ deleteButton.addEventListener("click", (e) => {
 // prendo la posizione del timer
 const timer = document.getElementById("timer")
 // mi faccio il timer
-let counter = 0
+let counter
+if (sessionStorage.getItem("timer")) {
+  counter = sessionStorage.getItem("timer")
+} else {
+  counter = 0
+}
 const counterUp = setInterval(() => {
+  counter++
   console.log(counter)
   timer.innerText = counter
-  return counter++
+  sessionStorage.setItem("timer", counter)
 }, 1000)
 // mi salvo in session storage il counter
-sessionStorage.setItem("timer", counter++)
